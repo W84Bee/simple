9bcf00ddd5505c04086bfabbf747b03a450a80d5
W84Bee <122640885+W84Bee@users.noreply.github.com>
2023-09-13 13:56:01 -0500

    Getting Help with Query Functions
    Helper Function
    - screen.logTestingPlaygroundURL()
    Testing Component
     - UserList.test.js
    Test Created
     - render one row per user
     - render email and name of user

---
+++ src/UserList.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import UserList from "./UserList";
+
+test('render one row per user', () => {
+    // Render the component
+    const users = [
+        { name: 'jane', email: 'jane@jane.com' },
+        { name: 'sam', email: 'sam@sam.com' }
+    ];
+    render(<UserList users={users}/>);
+
+    // Find all the rows in the table
+    const rows = screen.getAllByRole('row');
+
+    // Assertion: correct number of rows in the table
+    expect(rows).toHaveLength(2);
+});
+
+test('render the email and name of each user', () => {
+
+});
